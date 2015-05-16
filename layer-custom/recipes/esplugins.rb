2b271e3444490913034bfb7420e44da0f7b71325
Sacheendra Talluri <sacheendra.t@gmail.com>
2015-05-16 14:20:09 -0500

    install marvel instead of head

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -3,9 +3,9 @@ script "install_plugin_es_head" do
 	user "root"
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
-  	plugin -install mobz/elasticsearch-head
+  	plugin -install elasticsearch/marvel/latest
   	EOH
-	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/marvel") }
 end
 
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
